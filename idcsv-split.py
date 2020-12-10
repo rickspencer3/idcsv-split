aa5f286a4bfb1d8f81bc75b1372cf89c1a5e9ee5
Rick Spencer <richard.linger.spencer.3@gmail.com>
2020-12-10 11:52:43 -0500

    added CLI positional argument to take the filename

--- idcsv-split.py
+++ idcsv-split.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 
+import argparse
+
+parser = argparse.ArgumentParser(description='Split Flux CSV files exported from InfluxDB')
+parser.add_argument('filename', type=str, help='The name of the file to split.')
+args = parser.parse_args()
+filename = vars(args)["filename"]
+
 currentTable = 0
 currentFile = 0
 currentTable = 0
 
-fin = open('my-file.csv')
+fin = open(filename)
 
 fout = open('part{}.csv'.format(currentFile), 'a')
 
