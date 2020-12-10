5150c634bd0cef1ebba8bc8656d83a9d844efd2a
Rick Spencer <richard.linger.spencer.3@gmail.com>
2020-12-10 11:25:04 -0500

    first working version

---
+++ split.py
@@ -0,0 +1,19 @@
+currentTable = 0
+currentFile = 0
+currentTable = 0
+
+fin = open('taka-iot-data.csv')
+
+fout = open('part{}.csv'.format(currentFile), 'a')
+
+line = fin.readline()
+while line != None:
+    if line.startswith("#group"):
+        currentTable += 1
+        if currentTable % 100 == 0:
+            currentFile += 1
+            fout.close()
+            fout = open('part{}.csv'.format(currentFile), 'a')
+    fout.write(line)
+    line = fin.readline()
+
